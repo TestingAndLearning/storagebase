26f8326c765c373d46859d3c2a57af07180e1702
MT9304 <MT9304@gmail.com>
2016-10-15 23:37:17 -0700

    Finished basic retrieve function.

--- public/javascripts/createController.js
+++ public/javascripts/createController.js
@@ -5,46 +5,68 @@ $(document).ready(function()
         $('#test123').css("color", "yellow");
     });
     **/
-
-    var username = document.getElementById("username");
+/*
+    var claimnumber = document.getElementById("claimnumber");
     var firstname = document.getElementById("firstname");
     var lastname = document.getElementById("lastname");
     var email = document.getElementById("email");
-    var joined = document.getElementById("joined");
+    var date = document.getElementById("date");
     var submitBtn = document.getElementById("submitBtn");
-    var test123 = document.getElementById("test123");
+    */
 
    // var dbRef = firebase.database().ref().child('text');    //1A. Need this to initialize.
         //var firebaseRef = firebase.database().ref().child('text');  //1A. Also need this. But can also just use other way with jquery functions.
         //firebaseRef.set("Some Values");   //These 2 lines wre at function below.
 
+
+
+
+    //Stores form information into firebase. 
     $("#submitBtn").click(function(event){
+        firebase.database.enableLogging(true);
+        //test123.innerText = claimnumber;
+        var claimnumber = $("#claimnumber").val();
+        var firstname = $("#firstname").val();
+        var lastname = $("#lastname").val();
+        var email = $("#email").val();
+        var date = $("#date").val();
 
-        var username = document.getElementById("username");
-        var test321 = $("#username").val();
-        test123.innerText = test321;
-
-        firebase.database().ref('users/' + test321).set({
-            username: test321,
-            email: test321,
-            profile_picture : test321
+        firebase.database().ref('cases/' + claimnumber).set({
+            firstname: firstname,
+            lastname: lastname,
+            email : email,
+            date : date
         });
 
+    //Retrieves information from firebase. 
+    // Get a database reference to our posts
+    var db = firebase.database();
+    var ref = db.ref("cases/");
+    // Attach an asynchronous callback to read the data at our posts reference
+    ref.on("value", function(snapshot) {
+      console.log(snapshot.val());
+      test123.innerText = JSON.stringify(snapshot.val());
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
 
         event.preventDefault();
     });
 
 });
 
+
+
 /**
-var username = document.getElementById("username");
+var claimnumber = document.getElementById("claimnumber");
 var firstname = document.getElementById("firstname");
 var lastname = document.getElementById("lastname");
 var email = document.getElementById("email");
-var joined = document.getElementById("joined");
+var date = document.getElementById("date");
 var submitBtn = document.getElementById("submitBtn");
-**/
 
+
+test123.innerText = claimnumber;
 
 function submitClick()
 {
@@ -55,30 +77,30 @@ function submitClick()
 
 function writeUserData(userId, name, email, imageUrl) {
     firebase.database().ref('users/' + userId).set({
-        username: name,
+        claimnumber: name,
         email: email,
         profile_picture : imageUrl
     });
 }
 
-function createUser2(userId, firstname, email, joined)
+function createUser2(userId, firstname, email, date)
 {
     firebase.database().ref('users/' + userId).set({
-        username: firstname,
+        claimnumber: firstname,
         email: email,
-        profile_picture: joined
+        profile_picture: date
     });
 
     var test123 = document.getElementById("test123");
-    var username = document.getElementById("username");
-    test123.innerText = username;
+    var claimnumber = document.getElementById("claimnumber");
+    test123.innerText = claimnumber;
 
     event.preventDefault();
 }
 
 
 
-/**
+/
 function submitClick1()
 {
     var test123 = document.getElementById("test123");
