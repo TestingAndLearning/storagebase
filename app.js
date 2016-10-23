507d938bd3e23eb74386431e617c4848a1fd99b5
mt9304 <mt9304@gmail.com>
2016-10-22 23:20:23 -0700

    Added index, create, and navbar

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var create = require('./routes/create');
+var update = require('./routes/update');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/create', create);
+app.use('/update', update);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
