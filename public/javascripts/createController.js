2bb2c956467fda989daa46602d04b80ff173b26c
MT9304 <MT9304@gmail.com>
2016-10-22 18:50:16 -0700

    Starting navbar.

--- public/javascripts/createController.js
+++ public/javascripts/createController.js
@@ -1,26 +1,23 @@
 $(document).ready(function()
 {
+    //test123.innerText = claimnumber;
+    var claimnumber = $("#claimnumber").val();
+    var firstname = $("#firstname").val();
+    var lastname = $("#lastname").val();
+    var email = $("#email").val();
+    var date = $("#date").val();
+    
     populateTable();
-    var repopulate = false;
 
-    /** Saves new set of data.  **/
+    /** Checks for duplicate claim number, then sets new data.   **/
     $("#submitBtn").click(function(event)
     {
-        //firebase.database.enableLogging(true);
-        //test123.innerText = claimnumber;
-        var claimnumber = $("#claimnumber").val();
-        var firstname = $("#firstname").val();
-        var lastname = $("#lastname").val();
-        var email = $("#email").val();
-        var date = $("#date").val();
+        getFormData();
         var usersRef = firebase.database().ref("cases/").orderByKey();
-
-        /** Checks for duplicates for claimnumber. **/
         usersRef.once('value', function(snapshot) 
         {
             if(!snapshot.hasChild(claimnumber))
             {
-                populated = true;
                 firebase.database().ref('cases/' + claimnumber).set(
                 {
                     firstname: firstname,
@@ -31,37 +28,26 @@ $(document).ready(function()
             }
             else
             {
-                repopulate = false;
                 alert('Claim number already exists. ');
             }
         });
-
-        /** Displayed in table if not already loaded. **/
-        //TODO: Look into firebase.reload(); function. 
-        if (repopulate)
-        {
-            repopulate = false;
-            $("#claimsList tr").empty();
-            setTimeout(populateTable, 1000);    //Sets delay to 1000 milliseconds, if too fast then sometimes firebase does not get it fast enough. 
-            event.preventDefault(); //Prevents form from refreshing. 
-        }
+        event.preventDefault();
     });
 
 });
 
-function populateTable ()
+function getFormData()
 {
-    $('#claimsList').append
-    ('<table>'+
-        '<thead>'+
-        '<th>'+ 'Claim Number' +'</th>'+
-        '<th>'+ 'First Name' +'</th>'+
-        '<th>'+ 'Last Name' +'</th>'+
-        '<th>'+ 'Email' + '</th>'+
-        '<th>'+ 'Date' + '</th>'+
-        '</thead>'+
-    '</table>');
+    claimnumber = $("#claimnumber").val();
+    firstname = $("#firstname").val();
+    lastname = $("#lastname").val();
+    email = $("#email").val();
+    date = $("#date").val();
+}
 
+function populateTable()
+{
+    appendTableHeaders();
     // Loop through users in order with the forEach() method. The callback provided
     // to will be called synchronously with a DataSnapshot for each child:
     var query = firebase.database().ref("cases/").orderByKey();
@@ -83,3 +69,27 @@ function populateTable ()
             });
         });
 }
+
+function appendTableHeaders()
+{
+    $('#claimsList').append
+    ('<table>'+
+        '<thead>'+
+        '<th>'+ 'Claim Number' +'</th>'+
+        '<th>'+ 'First Name' +'</th>'+
+        '<th>'+ 'Last Name' +'</th>'+
+        '<th>'+ 'Email' + '</th>'+
+        '<th>'+ 'Date' + '</th>'+
+        '</thead>'+
+    '</table>');
+}
+
+        /**
+        if (repopulate)
+        {
+            repopulate = false;
+            $("#claimsList tr").empty();
+            setTimeout(populateTable, 1000);    //Sets delay to 1000 milliseconds, if too fast then sometimes firebase does not get it fast enough. 
+            event.preventDefault(); //Prevents form from refreshing. 
+        }
+        **/
